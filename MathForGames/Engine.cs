4a253db1947687ce128b90dbd668110d90ac07b1
Asigan <Zacharycgrippo@gmail.com>
2021-10-06 16:51:49 -0500

    Added initial classes and functions

---
+++ MathForGames/Engine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathForGames
+{
+    class Engine
+    {
+        private static bool _shouldApplicationClose = false;
+        private Scene[] _scenes = new Scene[0];
+        private static int _currentSceneIndex;
+
+        /// <summary>
+        /// Called to bein the application.
+        /// </summary>
+        public void Run()
+        {
+            //Call start for the entire application.
+            Start();
+
+            //Loop until the application is told to close.
+                while (!_shouldApplicationClose)
+                {
+                    Update();
+                    Draw();
+                }
+
+            // call end for the entire application.
+            End();  
+        }
+
+        /// <summary>
+        /// Called when the application starts
+        /// </summary>
+        private void Start()
+        {
+            _scenes[_currentSceneIndex].Start();
+        }
+
+        /// <summary>
+        /// Called everytime the game loops
+        /// </summary>
+        private void Update()
+        {
+            _scenes[_currentSceneIndex].Update();
+        }
+
+        /// <summary>
+        /// Called everytime the game loops to the update visuals
+        /// </summary>
+        private void Draw()
+        {
+            _scenes[_currentSceneIndex].Draw();
+        }
+        /// <summary>
+        /// Called when the application exits
+        /// </summary>
+        private void End()
+        {
+            _scenes[_currentSceneIndex].End();
+        }
+
+        /// <summary>
+        /// Addsa a scene to thje engine's scene array
+        /// </summary>
+        /// <param name="scene">the scene that will be added to the scene array</param>
+        /// <returns>the index where the new scene is located</returns>
+        public int AddScene(Scene scene)
+        {
+            //Creats a new temporary array
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+            //copies all values from the old array into the new array
+            for (int i = 0; i < _scenes.Length; i ++)
+            {
+                tempArray[i] = _scenes[i];
+            }
+            //Set the last index to be the new scene
+            tempArray[_scenes.Length] = scene;
+
+            //set the old array to be the new array
+            _scenes = tempArray;
+            //return the last array
+            return _scenes.Length - 1;
+        }
+    }
+}
---
+++ MathForGames/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathForGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
